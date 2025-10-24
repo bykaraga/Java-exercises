e981fa9f62dd8dfeaaeaaf5272c616e151a2e141
bykaraga <batuhan10demirboga@gmail.com>
2025-10-24 23:22:13 +0200

    Add files via upload

---
+++ java egzersiz/src/oop/siniflar.java
@@ -0,0 +1,17 @@
+package oop;
+
+public class siniflar {
+    public static void main(String[] args){
+        //object oriented Programming
+        //sinifAdi objeAdi = new SinifAdi();
+
+        variables var1 = new variables();
+        var1.sayi1 = 10;
+        var1.sayi2=20;
+        System.out.println(var1.sayi1);
+
+        variables var2 = new variables();
+        var2.sayi1=20;
+        System.out.println(var2.sayi1);
+    }
+}
---
+++ java egzersiz/src/oop/variables.java
@@ -0,0 +1,7 @@
+package oop;
+
+public class variables {
+  public  int sayi1;
+   public   int sayi2;
+
+}
