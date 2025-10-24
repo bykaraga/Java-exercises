92a5d67423612d25939c2b2cbe2bcd8267d95fbe
bykaraga <batuhan10demirboga@gmail.com>
2025-10-24 23:05:43 +0200

    Add files via upload

---
+++ java egzersiz/src/toDo/fibonacci.java
@@ -0,0 +1,20 @@
+package toDo;
+
+public class fibonacci {
+
+
+    static int fibo(int n){
+
+if(n == 1 || n == 2)
+{ return 1;}
+
+return fibo(n-1)+fibo(n-2);
+    }
+
+
+    public static void main(String[] args){
+// 1 1 2 3 5 8 13 21
+        System.out.println(fibo(7));
+    }
+
+}
--- java egzersiz/src/toDo/palindrom.java
+++ java egzersiz/src/toDo/palindrom.java
@@ -1,4 +1,25 @@
 package toDo;
 
 public class palindrom {
+    //123321 121
+
+    static boolean isPalindrom(int input){
+       int tmp,tersSayi=0,kalan;
+       tmp =input;
+
+       while(tmp!=0){
+           kalan = tmp%10;
+           tersSayi = tersSayi*10 +kalan;
+           tmp /=10;
+
+       }
+       if(input == tersSayi){return true;}
+       else
+        return false;
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(isPalindrom(12321));
+    }
 }
